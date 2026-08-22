85c1f5949b7d106ec550c1cba68f47631e8e7d38
GabrielLazuli <gssantos1.cic@uesc.br>
2026-08-21 23:27:44 -0300

    componentes em react

---
+++ my-profile/src/components/Profile.tsx
@@ -0,0 +1,10 @@
+
+export default function Profile(){
+    return( 
+        <div>
+            <img src="https://media.licdn.com/dms/image/v2/D4D03AQEtYK80ZBt1Vw/profile-displayphoto-scale_400_400/B4DZ89z8csIEAg-/0/1783448460146?e=1788998400&v=beta&t=7eMnoxoFIFcBjuOZK_-aanLbMgSTemN8ECbysKZYXPg" alt="" />
+            <h3>Gabriel Souza Santos</h3>
+            <h4>Estudante de Ciência da Computação | Dev Front-End</h4>
+        </div>
+    )
+}
