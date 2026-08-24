b952d9fee0c64b326dc51f491c608615ccb7d696
GabrielLazuli <gssantos1.cic@uesc.br>
2026-08-23 23:12:16 -0300

    estudos em TSX

--- treinos-em-react/my-profile/src/components/Profile.tsx
+++ treinos-em-react/my-profile/src/components/Profile.tsx
@@ -1,13 +1,15 @@
 import {Skills} from './Skills'
 
 export function Profile(){
+
+     const nome: string = "Gabriel Souza";
+
     return( 
         <div>
             <img src="https://media.licdn.com/dms/image/v2/D4D03AQEtYK80ZBt1Vw/profile-displayphoto-scale_400_400/B4DZ89z8csIEAg-/0/1783448460146?e=1788998400&v=beta&t=7eMnoxoFIFcBjuOZK_-aanLbMgSTemN8ECbysKZYXPg" alt="" />
-            <h3>Gabriel Souza Santos</h3>
+            <h3>{nome} Santos </h3>
             <h4>Estudante de Ciência da Computação | Dev Front-End</h4>
             <Skills/>
-
         </div>
     )
 }
--- treinos-em-react/my-profile/src/components/typeSkills/HardSkills.tsx
+++ treinos-em-react/my-profile/src/components/typeSkills/HardSkills.tsx
@@ -1,7 +1,15 @@
 
+interface ImgOptions{
+    color: string;
+}
+
 export function HardSkills(){
+
+    const listaOptions: ImgOptions = { color: "red"};
+    
+
     return(
-        <ul>
+        <ul style={{color:listaOptions.color}}>
             <li>HTML</li>
             <li>CSS</li>
             <li>TS</li>
