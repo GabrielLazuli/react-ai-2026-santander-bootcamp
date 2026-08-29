a76665b1121a8a58f55d0c8135766a9732bf289b
GabrielLazuli <gssantos1.cic@uesc.br>
2026-08-29 14:28:36 -0300

    map

--- treinos-em-react/cra-project/src/App.js
+++ treinos-em-react/cra-project/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
-import Saudacao from './Saudacao';
+import Produtos from './components/Produtos';
 
-const saudacao = "Hello World";
+const produtos = [
+  {
+    id: 1,
+    nome: "TV",
+    descricao: "aparalho de 5 polegadas"
+  },
+  {
+    id: 2,
+    nome: "Tablet",
+    descricao: "7 polegadas"
+  },
+  {
+    id: 3,
+    nome: "Celular",
+    descricao: "um bom celular"
+  }
+]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            <Saudacao oi={saudacao}/>
-        </p>
-        <a 
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     
+    {produtos.map((produto) => (
+      <div key={produto.id}>
+          <Produtos name={produto.nome} descricao={produto.descricao}/>
+      </div>
+      ))}
+
       </header>
     </div>
   );
---
+++ treinos-em-react/cra-project/src/components/Produtos.jsx
@@ -0,0 +1,12 @@
+
+
+function Produtos({name, descricao}){
+    return(
+        <div>
+            <p>{name}</p>
+            <p>{descricao}</p>
+        </div>
+    );
+}
+
+export default Produtos;
