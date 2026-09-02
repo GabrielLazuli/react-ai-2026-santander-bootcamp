0fdd1bf35e51318590ea03e29514d8e726d7259b
GabrielLazuli <gssantos1.cic@uesc.br>
2026-09-02 19:42:49 -0300

    useefect

--- treinos-em-react/cra-project/src/components/Produtos.jsx
+++ treinos-em-react/cra-project/src/components/Produtos.jsx
@@ -1,13 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 
 function Produtos(){
 
-    const [quantidade, setquantiade] = useState(0);
+    const [quantidade, setquantiade] = useState(0)
+
+
+    useEffect(()=> {
+
+        document.title = `voce clicou ${quantidade}`;
+
+    }, [quantidade]
+)
+
+
 
      return(
         <div>
+
+                <p>Tema alterado</p>
+
             <p>Quantidade {quantidade}</p>
             <button onClick={ () => setquantiade( quantidade + 1)}>adicionar</button>
         </div>  
---
+++ treinos-em-react/cra-project/src/components/TemaAlterado.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+
+export default function TemaAlterado() { 
+    
+    const[trocartema, setrocartema] = useState(false)
+
+    return(
+        
+    )
+
+}
+
